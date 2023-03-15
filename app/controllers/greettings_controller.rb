497219059bb861601067577a93ae9219162c9ec3
gandradep <eventsonido@gmail.com>
2023-03-15 02:12:01 -0600

    Create random greetting function, tested api in port 3001

--- app/controllers/greettings_controller.rb
+++ app/controllers/greettings_controller.rb
@@ -1,51 +1,7 @@
 class GreettingsController < ApplicationController
-  before_action :set_greetting, only: %i[ show update destroy ]
-
   # GET /greettings
   def index
-    @greettings = Greetting.all
-
-    render json: @greettings
+    surprise = Greetting.find(Greetting.pluck(:id).sample)
+    render json: surprise
   end
-
-  # GET /greettings/1
-  def show
-    render json: @greetting
-  end
-
-  # POST /greettings
-  def create
-    @greetting = Greetting.new(greetting_params)
-
-    if @greetting.save
-      render json: @greetting, status: :created, location: @greetting
-    else
-      render json: @greetting.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /greettings/1
-  def update
-    if @greetting.update(greetting_params)
-      render json: @greetting
-    else
-      render json: @greetting.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /greettings/1
-  def destroy
-    @greetting.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_greetting
-      @greetting = Greetting.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def greetting_params
-      params.require(:greetting).permit(:text)
-    end
 end
--- test/controllers/greettings_controller_test.rb
+++ test/controllers/greettings_controller_test.rb
@@ -1,35 +1,35 @@
-require "test_helper"
+require 'test_helper'
 
 class GreettingsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @greetting = greettings(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get greettings_url, as: :json
     assert_response :success
   end
 
-  test "should create greetting" do
-    assert_difference("Greetting.count") do
+  test 'should create greetting' do
+    assert_difference('Greetting.count') do
       post greettings_url, params: { greetting: { text: @greetting.text } }, as: :json
     end
 
     assert_response :created
   end
 
-  test "should show greetting" do
+  test 'should show greetting' do
     get greetting_url(@greetting), as: :json
     assert_response :success
   end
 
-  test "should update greetting" do
+  test 'should update greetting' do
     patch greetting_url(@greetting), params: { greetting: { text: @greetting.text } }, as: :json
     assert_response :success
   end
 
-  test "should destroy greetting" do
-    assert_difference("Greetting.count", -1) do
+  test 'should destroy greetting' do
+    assert_difference('Greetting.count', -1) do
       delete greetting_url(@greetting), as: :json
     end
 
